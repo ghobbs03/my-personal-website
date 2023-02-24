2106069602215f8e4098c7c736f2654f2d270988
Gail Hobbs <gbhobbs97@gmail.com>
2023-02-23 17:14:43 -0800

    updated resume

---
+++ script.js
@@ -0,0 +1,5 @@
+/*document.querySelector("#aboutme").addEventListener('click', () => aboutMeHandler());
+
+function aboutMeHandler() {
+    document.querySelector("#outer").style.visibility = 'hidden';
+}*/
