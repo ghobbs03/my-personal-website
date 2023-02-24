8a6053eaf698d85e129fd53f345fb67d8668b67c
Gail Hobbs <gbhobbs97@gmail.com>
2023-02-24 11:17:43 -0800

    updated aboutme

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.querySelector("#face").addEventListener('click', () => aboutMeHandler());
+document.querySelector("#aboutme").addEventListener('click', () => aboutMeHandler());
 
 let isAboutMeShowing = false;
 const bio = document.querySelector('#bio');
@@ -16,7 +17,8 @@ function aboutMeHandler() {
         bio.innerHTML =
         `<center>Gail Hobbs is a junior software developer. 
         In her free time she enjoys singing, reading, solving chess puzzles and cryptic crosswords, 
-        eating ramen, and watching movies that give her heart palpitations. Feel free to stay a while.</center>
+        eating ramen, and watching movies that give her heart palpitations. You can find out more about how I think and work <a href="https://secure.plum.io/en/p/LMTOJ-j9iaxqKfq7qr1dFg" style="color: midnightblue" target="_blank" rel="noopener noreferrer"> here.</a>
+        <br>Feel free to look around.</center>
         `;
 
         bio.style.visibility = "visible";
