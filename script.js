fb17d06bfa267e9d51f914f83f0b5267c21b4300
Gail Hobbs <gbhobbs97@gmail.com>
2023-09-12 12:22:35 -0700

    new headline

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-document.querySelector("#face").addEventListener('click', () => aboutMeHandler());
-document.querySelector("#aboutme").addEventListener('click', () => aboutMeHandler());
+document
+  .querySelector("#face")
+  .addEventListener("click", () => aboutMeHandler());
+document
+  .querySelector("#aboutme")
+  .addEventListener("click", () => aboutMeHandler());
 
 let isAboutMeShowing = false;
-const bio = document.querySelector('#bio');
-
+const bio = document.querySelector("#bio");
 
 function aboutMeHandler() {
-    const infoContainer = document.querySelector("body");
-    infoContainer.append(bio);
+  const infoContainer = document.querySelector("body");
+  infoContainer.append(bio);
 
+  isAboutMeShowing = !isAboutMeShowing;
+  const name = document.querySelector("h2");
 
-    isAboutMeShowing = !isAboutMeShowing;
-    const name = document.querySelector("h2");
-
-    if (isAboutMeShowing) {
-        bio.innerHTML =
-        `Gail Hobbs is a junior developer. 
+  if (isAboutMeShowing) {
+    bio.innerHTML = `Gail Hobbs is a software engineer. 
         In her free time she enjoys singing, reading, eating ramen, making animations like the one you see here, solving chess puzzles and cryptic crosswords,  and watching movies that induce heart palpitations. You can find out more about how I think and work <a href="https://secure.plum.io/en/p/LMTOJ-j9iaxqKfq7qr1dFg" style="color: midnightblue" target="_blank" rel="noopener noreferrer"> here.</a>
          Feel free to click around.
         `;
 
-        bio.style.visibility = "visible";
-        
-
-    } else {
-        bio.style.visibility = "hidden";
-    }
-
-
-
-
+    bio.style.visibility = "visible";
+  } else {
+    bio.style.visibility = "hidden";
+  }
 }
